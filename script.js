c7a407456e00a231a0424d119dc7b15a4c3b9ab6
Curtis <curtis@Curtiss-MacBook-Pro.local>
2021-03-04 10:45:01 -0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// View Highscores page top left
+// Timer top right
+// Landing page with intro and start button
+// On click begin questions
+// Array of questions
+// Each question has 4 buttons as answers
